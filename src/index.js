1e931313a0a8e98cc548b17c2ced097492f41793
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2026-01-27 11:27:48 -0500

    deleted index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-export { pca_UI, pca } from "./pca.js";
-export { heatmap } from "./heatmap.js";
-export { hclust_plot, hclust } from "./hclust.js";
-
-// Optional: version tag
-export const version = "0.1.0";
